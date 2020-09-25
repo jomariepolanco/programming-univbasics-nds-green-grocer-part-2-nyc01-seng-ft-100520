a88edf7d955905f40a8e04f429ec95066f9bc7e9
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-25 13:45:05 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,29 +6,23 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
- counter = 0 
-  while counter < coupons.length 
-    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    couponed_item_name = "#{coupons[counter][:item]} W/ COUPON"
-    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-    if cart_item && cart_item[:count] >= coupons[counter][:num]
-      if cart_item_with_coupon
-        cart_item_with_coupon[:count] += coupons[counter][:num]
-        cart_item[:count] -= coupons[counter][:num]
-      else 
-        cart_item_with_coupon = {
-          :item => couponed_item_name,
-          :price => coupons[counter][:cost] / coupons[counter][:num],
-          :count => coupons[counter][:num],
-          :clearance => cart_item[:clearance]
+ coupons.each do |coupon|
+    item_in_cart = find_item_by_name_in_collection(coupon[:item], cart)
+    cart_item_w_coupon = find_item_by_name_in_collection(coupon[:item] + " W/COUPON", cart)
+    if cart_item_w_coupon && item_in_cart[:count] >= coupon[:num]
+        cart_item_w_coupon[:count] += coupon[:num]
+        item_in_cart[:count] -= coupon[:num]
+       elsif item_in_cart && item_in_cart[:count] >= coupon[:num]
+        cart << {
+          :item => coupon[:item] + " W/COUPON",
+          :price => coupon[:cost] / coupon[:num],
+          :count => coupon[:num],
+          :clearance => item_in_cart[:clearance]
         }
-        cart.push(cart_item_with_coupon)
-        cart_item[:count] -= coupons[counter][:num]
-      end
-    end 
-    counter += 1 
-  end 
-  cart 
+        item_in_cart[:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
